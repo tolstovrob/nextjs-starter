4377fda0fb8e09d78fc3f6e2e94205686d4ddcc8
tolstovrob <tolstovrob@gmail.com>
2025-01-26 23:11:12 +0400

    chore: ignore .next and node_modules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,11 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript')];
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    ignores: ['.next/', 'node_modules/'],
+  },
+];
 
 export default eslintConfig;
